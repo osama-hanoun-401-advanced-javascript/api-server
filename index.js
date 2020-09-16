9a9fc3f5e2e77e85632b6defac170a1622ee31b5
Osama Hanoun <osamahanoun997@gmail.com>
2020-09-16 08:18:47 +0300

    finish 65% of class-08

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
+const mongoose = require('mongoose');
 const serverModule = require('./lib/server.js');
 require('dotenv').config();
 let port =process.env.PORT;
+
+const MONGOOSE_URL = 'mongodb://localhost:27017/food-db';
+
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(MONGOOSE_URL, mongooseOptions);
+
 serverModule.start(port);
