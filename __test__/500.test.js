fe63d63856b07c450a247c09da5f6b656c9f596a
Osama Hanoun <osamahanoun997@gmail.com>
2020-09-16 16:00:07 +0300

    fix eslint

--- __test__/500.test.js
+++ __test__/500.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 'use strict';
 
 const _500Middleware = require('../lib/middleware/500.js');
@@ -5,19 +7,19 @@ const express = require('express');
 const supertest = require('supertest');
 const app = express();
 app.get('/test500Middleware', function (req, res) {
-    try{throw 500} catch(err){next(err)}
+  try{throw 500;} catch(err){next(err);}
 },_500Middleware);
 
 describe('500 Middleware', () => {
 
-    it('properly send and status requests', () => {
-        return supertest(app).get('/test500Middleware')
-            .then(result => {
-                expect(result.status).toBe(500);
-                expect(result.text).toBe('Error 500');
+  it('properly send and status requests', () => {
+    return supertest(app).get('/test500Middleware')
+      .then(result => {
+        expect(result.status).toBe(500);
+        expect(result.text).toBe('Error 500');
 
-            })
-    });
+      });
+  });
 
 });
 
--- __test__/timestamp.test.js
+++ __test__/timestamp.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 'use strict';
 
 const timestamp = require('../lib/middleware/timestamp.js');
@@ -6,18 +7,18 @@ const supertest = require('supertest');
 const app = express();
 app.use(timestamp);
 describe('timestamp Middleware', () => {
-    let date;
-    app.get('/testTimestampMiddleware', function (req, res) {
-        date = res.requestTime;
-        res.end();
-    });
-    it('properly puts the current timestamp on the request', () => {
-        return supertest(app).get('/testTimestampMiddleware')
-            .then(() => {
-                expect(date.includes('GMT')).toBeTruthy();
-            })
+  let date;
+  app.get('/testTimestampMiddleware', function (req, res) {
+    date = res.requestTime;
+    res.end();
+  });
+  it('properly puts the current timestamp on the request', () => {
+    return supertest(app).get('/testTimestampMiddleware')
+      .then(() => {
+        expect(date.includes('GMT')).toBeTruthy();
+      });
 
-    });
+  });
 });
 
 
